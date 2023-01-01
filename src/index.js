6eefd24d276f9a26e16dff0e88d9fc548b2f973b
danilov <danilovaleksey89@gmail.com>
2023-01-01 18:49:11 +0300

    fix package.json and enrty

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
 import { Provider } from "react-redux";
 import store from './store/configureStore';
 
-ReactDOM.render(
-    <Provider store={store}>
-      <App />
-    </Provider>,
-    document.getElementById('root')
+createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
-
